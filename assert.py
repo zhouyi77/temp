c4f592eac4b1d6859bf54250bde529ddcb4c63bb
yingqi <1069743654@qq.com>
2021-09-24 09:46:37 +0800

    temp

---
+++ assert.py
@@ -0,0 +1,12 @@
+import pytest
+# 详细断言异常
+def test_zero_division_long():
+    with pytest.raises(ZeroDivisionError) as excinfo:
+        var = 1 / 0
+
+    # 断言异常类型 type
+    assert excinfo.type == ZeroDivisionError
+    # 断言异常 value 值
+    assert "zero" in str(excinfo.value)
+
+
